4f5742f566e82edf20b1b4a5fee23d535e91563b
David Mahoney <dmahoney@baskalisk.ca>
2024-01-25 21:16:57 -0800

    Added missing file

--- tutorial.gd
+++ tutorial.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 @onready var timer: Timer = get_node("MessageTimer")
 
 var firstPlayeMoved = false
+var first_energy = true
 
 func _ready():
 	ev.something_happened.connect(game_changed)
@@ -15,10 +16,35 @@ func game_changed(evt):
 	if evt == "tutorial_basic_movement":
 		handle_player_moved()
 	elif evt == "tutorial_energy":
+		energy()
+	elif evt == "tutorial_eating_energy":
 		eating_energy()
+	elif evt == "tutorial_toxic_floor":
+		toxic_floor()
+	elif evt == "tutorial_toxic_floor":
+		vibrant_floor()
+
+func vibrant_floor():
+	message.text = "Easy going. this floor takes less energy to move"
+	timer.start()
+	show()
+	
+func toxic_floor():
+	message.text = "Hard going. This floor takes more energy to move"
+	timer.start()
+	show()
+	
+func energy():
+	message.text = "Movement takes energy. Find something to eat!"
+	timer.start()
+	show()
 
 func eating_energy():
-	message.text = "Movement takes energy. Find something to eat!"
+	if first_energy:
+		message.text = "Mmm, chicken. Wait. Why does it taste like chicken?"
+		first_energy = false
+	else:
+		message.text = "Oh, yeah. That's some good energy"
 	timer.start()
 	show()
 	
